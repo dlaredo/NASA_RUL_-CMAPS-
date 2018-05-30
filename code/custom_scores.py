Fixing RHS computation to comply with the original definition

code/custom_scores.py
@@ -3,13 +3,20 @@ import numpy as np
 
 def rul_health_score(y_true, y_pred):
     s=0
-    for i in range(len(y_true)):
-        d = y_pred[i] - y_true[i]
-        if d < 0:
-            s+=math.e**(-d/13)-1
+
+    d = y_pred - y_true
+
+    print(d)
+
+    for i in range(len(d)):
+        if d[i] < 0:
+            s+=math.e**(-d[i]/13)-1
         else:
-            s+=math.e**(d/10)-1
-    return np.asscalar(s)
+            s+=math.e**(d[i]/10)-1
+
+    s = s/len(d)
+
+    return s
 
 score_dict = {'rhs':lambda x,y : rul_health_score(x,y)}
 

code/tunableModel.py
@@ -106,9 +106,9 @@ class TunableModel():
 		self.X_train = X_train
 		self.X_crossVal = X_crossVal
 		self.X_test = X_test
-		self.y_train = y_train
-		self.y_crossVal = y_crossVal
-		self.y_test = y_test
+		self.y_train = np.ravel(y_train)
+		self.y_crossVal = np.ravel(y_crossVal)
+		self.y_test = np.ravel(y_test)
 		self.__trimmedRUL_train = trimmedRUL_train
 
 		if verbose == 1:
@@ -264,31 +264,31 @@ class TunableModel():
 			
 			print("Training data (X, y)")
 			print(self.X_train[:5,:])
-			print(self.y_train[:5,:])
+			print(self.y_train[:5])
 
 			if self.X_crossVal is not None:
 				print("Cross-Validation data (X, y)")
 				print(self.X_crossVal[:5,:])
-				print(self.y_crossVal[:5,:])
+				print(self.y_crossVal[:5])
 
 			print("Testing data (X, y)")
 			print(self.X_test[:5,:])
-			print(self.y_test[:5,:])
+			print(self.y_test[:5])
 		else:
 			print("Printing last 5 elements\n")
 			
 			print("Training data (X, y)")
 			print(self.X_train[-5:,:])
-			print(self.y_train[-5:,:])
+			print(self.y_train[-5:])
 
 			if self.X_crossVal is not None:
 				print("Cross-Validation data (X, y)")
 				print(self.X_crossVal[-5:,:])
-				print(self.y_crossVal[-5:,:])
+				print(self.y_crossVal[-5:])
 
 			print("Testing data (X, y)")
 			print(self.X_test[-5:,:])
-			print(self.y_test[-5:,:])
+			print(self.y_test[-5:])
 
 
 	#property definition